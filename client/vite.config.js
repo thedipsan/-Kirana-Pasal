288bdb2435c32dcb34e1a28d46cb318f6d28d10b
Dipsan Dhital <dipsandhital12@gmail.com>
2026-08-31 21:17:52 +0545

    Creating Navbar Components

--- client/vite.config.js
+++ client/vite.config.js
@@ -3,5 +3,5 @@ import { defineConfig } from 'vite'
 import tailwindcss from '@tailwindcss/vite'
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react(),tailwindncss()],
+  plugins: [react(), tailwindcss()],
 })
